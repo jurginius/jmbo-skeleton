89298454dce5fcb150f3e6e1d5697568c55ad376
Jurgen Blignaut <jurgen@webtide.co.za>
2013-03-26 18:23:00 +0200

    Updated Everlytic list id setting

--- skeleton/settings_dev_basic_site.py
+++ skeleton/settings_dev_basic_site.py
@@ -10,5 +10,5 @@ INSTALLED_APPS = INSTALLED_APPS + ('everlytic',)
 EVERLYTIC = {
     'URL': 'http://praekelt-host2.pmailer.net/api/1.0',
     'API_KEY': 'DwnGAw4ISfcqLmqnGRYrXOeJw8L3eJBS',
-    'LIST_ID': 12701    # ECR Subscriptions
+    'LIST_ID': 3780    # ECR Subscriptions Test
 }
